67d08897efec779fab3a88122098ad7dda994754
Gage Renzi <gage.renzi@healthstream.com>
2018-06-25 15:59:12 -0500

    adding exclusion list, measure hooks

--- tmsl_standardizer.py
+++ tmsl_standardizer.py
@@ -5,17 +5,31 @@
 __author__      = "Gage Renzi"
 __copyright__   = "Copyright 2009, Planet Earth"
 __license__ = "GPLv3"
-__version__ = "0.0.2"
+__version__ = "0.0.1"
 
 import json
 import click
+import logging
+
+logger = logging.getLogger('tmsl_standardizer')
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(message)s',
+                    handlers=[logging.StreamHandler()]) #or, or add logging.FileHandler("example1.log")
+
+# place column or measure names in respective arrays to exclude them
+exclude_columns = []
+exclude_measures = []
 
 g={}
 # see this for tmsl reference for model definition
 # https://docs.microsoft.com/en-us/sql/analysis-services/tabular-models-scripting-language-objects/tables-object-tmsl?view=sql-analysis-services-2017
 
-def standardize_date_format(col):    
+# See this for formatString info:
+# https://docs.microsoft.com/en-us/sql/analysis-services/multidimensional-models/mdx/mdx-cell-properties-format-string-contents?view=sql-analysis-services-2017
+
+def standardize_column_date_format(col):    
     if col['dataType']=='dateTime':
+        logger.info('Date Standardizing Column {0}'.format(col['name']))
         col['formatString']= 'Short Date'
         col['annotations']=[
               {
@@ -24,22 +38,31 @@ def standardize_date_format(col):
               }
             ]
 
-# From: https://docs.microsoft.com/en-us/sql/analysis-services/multidimensional-models/mdx/mdx-cell-properties-format-string-contents?view=sql-analysis-services-2017
-# Numeric Values
-# A user-defined format expression for numbers can have anywhere from one to four sections separated by semicolons. If the format argument contains one of the named numeric formats, only one section is allowed.
+def standardize_measure_date_format(m):    
+    if 'date' in m['name'].lower():
+        logger.info('Date Standardizing Measure {0}'.format(col['name']))
+        m['formatString']= 'Short Date'
+        m['annotations']=[
+              {
+                "name": "Format",
+                "value": "<Format Format=\"DateTimeShortDatePattern\" />"
+              }
+            ]
 
-# Usage	Result
-# One section	The format expression applies to all values.
-# Two sections	The first section applies to positive values and zeros, the second to negative values.
-# Three sections	The first section applies to positive values, the second to negative values, and the third to zeros.
-# Four sections	The first section applies to positive values, the second to negative values, the third to zeros, and the fourth to null values.
-def standardize_whole_number_commas(col):
+def standardize_column_whole_number_commas(col):
     if col['dataType']=='int64':
+        logger.info('Whole Number Standardizing Column {0}'.format(col['name'])
         col['formatString']= '#,0;(-#,0)'
 
-def standardize_percentiles(col):
+def standardize_column_percentiles(col):
     if col['dataType']=='double' and '%' in col['name']:
+        logger.info('Percentile Standardizing Column {0}'.format(col['name']))
         col['formatString']= '0.0%;(-0.0%);0.0%'    
+
+def standardize_measure_percentiles(m):
+    if '%' in m['name']:
+        logger.info('Percentile Standardizing Measure {0}'.format(col['name']))
+        m['formatString']= '0.0%;(-0.0%);0.0%'    
  
 
 ################################################################################
@@ -54,13 +77,17 @@ def main(infile, outfile):
 
     #iterate through tables and pass the columns off for each function to take a crack at
     for tab in model['model']['tables']:
-        for col in tab['columns']:
-            standardize_date_format(col)
-            standardize_whole_number_commas(col)
-            standardize_percentiles(col)
+        for [col in tab['columns'] if col not in exclude_columns]:
+            standardize_column_date_format(col)
+            standardize_column_whole_number_commas(col)
+            standardize_column_percentiles(col)
+        if 'measures' in tab.keys():
+            for [measure in tab['measures'] if measure not in exclude_measures]:
+                standardize_measure_date_format(measure)
+                standardize_measure_percentiles(measure)
     
     #save output pretty printed, 2 spaces is what visual studio does
-    outfile.write(json.dumps(model, indent=2, sort_keys=False))
+    outfile.write(json.dumps(model, indent=2))
 
 if __name__ == '__main__':
     main()
